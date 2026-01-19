7882fcc12b0d0edeebe63eaceb8d888a2e823618
Tahmid Tameem <tahmidtameem2017@gmail.com>
2026-01-19 15:02:02 +0300

    add import db details from json

--- db.py
+++ db.py
@@ -1,11 +1,16 @@
 import mysql.connector
+import json
+
+with open("db.json","r",encoding="utf-8") as f :
+    data = json.load(f)
+
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        port=3306,
-        user="vscode",
-        password="123456",
-        database="CoffeeShopDB"
+        host=data["host"],
+        port=data["port"],
+        user=data["user"],
+        password=data["password"],
+        database=data["database"]
 
     )
